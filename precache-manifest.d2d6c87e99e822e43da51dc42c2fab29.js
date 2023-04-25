6d163010ce5ca6731712ae99d9abd9c8fd842ab7
Noor ul Ain <noorulainshakeelahmed@gmail.com>
2023-04-26 04:49:56 +0500

    Updates

---
+++ precache-manifest.d2d6c87e99e822e43da51dc42c2fab29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5f5b73223518f03d74f7",
+    "url": "/periodic-table/static/css/main.8295e09a.chunk.css"
+  },
+  {
+    "revision": "5f5b73223518f03d74f7",
+    "url": "/periodic-table/static/js/main.5f5b7322.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/periodic-table/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "96e33af574a792f07e84",
+    "url": "/periodic-table/static/js/2.96e33af5.chunk.js"
+  },
+  {
+    "revision": "c24b67e211701bbde482",
+    "url": "/periodic-table/static/js/runtime~main.c24b67e2.js"
+  },
+  {
+    "revision": "0304dae1000d1b48b6550bef3d7211da",
+    "url": "/periodic-table/index.html"
+  }
+];
